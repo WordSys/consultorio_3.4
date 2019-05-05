cddd2498d4d5365b502a845445fa386758cfc02a
Walter Ordoñez <32498310+WordSys@users.noreply.github.com>
2019-05-05 19:20:20 -0300

    ingrso de usuarios

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,4 +23,20 @@ class DefaultController extends Controller
         */
 
     }
+
+    /**
+     * @Route("/", name="ingresar")
+     */
+    public function ingresarAction(Request $request)
+    {
+
+        return $this->render('home/ingresar.html.twig');
+        // replace this example code with whatever you need
+        /*
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+        */
+
+    }
 }
