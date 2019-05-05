032f0ef03a819778050fcebb3f9ad3b25d467ce7
Walter Ordoñez <32498310+WordSys@users.noreply.github.com>
2019-05-05 17:06:53 -0300

    Comenzando a escribir paginas

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        return $this->render('home/index.html.twig');
         // replace this example code with whatever you need
+        /*
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+        */
+
     }
 }
