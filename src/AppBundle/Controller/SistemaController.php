975f895272d0896b33db36fbad83e74bf074a867
Walter Ordoñez <32498310+WordSys@users.noreply.github.com>
2019-05-05 20:01:57 -0300

    commit estructura de menu

---
+++ src/AppBundle/Controller/SistemaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SistemaController extends Controller
+{
+    /**
+     * @Route("/sistema/profesionales", name="profesionales")
+     */
+    public function profesionalesAction(Request $request)
+    {
+
+        return $this->render('sistema/profesionales.html.twig');
+        // replace this example code with whatever you need
+        /*
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+        */
+
+    }
+
+
+}
